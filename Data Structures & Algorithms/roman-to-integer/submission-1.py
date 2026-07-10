456fefb35a1e1ecd8243c9c44a283f2f8a4ebbdf
ASWINN-M <149362578+ASWINN-M@users.noreply.github.com>
2026-07-10 22:18:46 +0530

    Add: roman-to-integer - submission-1

---
+++ Data Structures & Algorithms/roman-to-integer/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        res = 0
+        i = 0
+        while i < len(s) - 1:
+            if roman[s[i]] < roman[s[i + 1]]:
+                res += roman[s[i + 1]]
+                res -= roman[s[i]]
+                i += 1
+                print(res)
+            else:
+                res += roman[s[i]]
+            i += 1
+        
+        if i < len(s):
+            res += roman[s[i]]
+        return res
+        
